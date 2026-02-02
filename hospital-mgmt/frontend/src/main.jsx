9c1a26f2cba3f964ec7ad0b5de12209aad5894be
Ravish052 <bhatnagar463@gmail.com>
2026-02-02 23:18:36 +0530

    initialized routes

--- hospital-mgmt/frontend/src/main.jsx
+++ hospital-mgmt/frontend/src/main.jsx
@@ -1,10 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { createContext, useState } from 'react'
 import App from './App.jsx'
+
+export const Context = createContext({isAuthenticated : false});
+
+const AppWrapper = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [user, setUSer] = useState(null);
+
+  return (
+    <Context.Provider value = {{isAuthenticated, setIsAuthenticated, user, setUSer}}>
+      <App />
+    </Context.Provider>
+  )
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <AppWrapper />
   </StrictMode>,
 )
